d8b6ef8aae18bd803f76912bdf6a960cbeaa226c
Bernardo Lins <bernardolins28@gmail.com>
2016-02-07 03:32:17 -0200

    Create main command named clue

--- cmd/clue/clue.go
+++ cmd/clue/clue.go
@@ -1,30 +1,16 @@
-package cmd
+package clue
 
 import (
 	"github.com/spf13/cobra"
 )
 
-type Clue struct {
-	command *cobra.Command
+var Command = &cobra.Command{
+	Use: "clue",
+	Run: func(cmd *cobra.Command, args []string) {
+		printHelp()
+	},
 }
 
-func NewClue() *Clue {
-	clue := new(Clue)
-
-	clue.command = &cobra.Command{
-		Use: "clue",
-		Run: func(cmd *cobra.Command, args []string) {
-			clue.printHelp()
-		},
-	}
-
-	return clue
-}
-
-func (clue *Clue) Command() *cobra.Command {
-	return clue.command
-}
-
-func (clue *Clue) printHelp() {
+func printHelp() {
 
 }
