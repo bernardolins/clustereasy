5e0cab006efa2a012a8730fa9df39f186514dc6d
Bernardo Lins <bernardolins28@gmail.com>
2016-02-07 15:28:35 -0200

    Fix wrong workdir path on compile script

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ GOVERSION=1.5
 ## Compile with docker
 docker run --rm \
   -v $(pwd):/go/src/github.com/bernardolins/clustereasy/ \
-  -w go/src/github.com/bernardolins/clustereasy \
+  -w /go/src/github.com/bernardolins/clustereasy \
   golang:$GOVERSION \
   go get ./... && \
   go build -v
