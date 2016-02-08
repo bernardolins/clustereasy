bc64bfb516456bcebe82f5b9d1c89f11b14bd776
Bernardo Lins <bernardolins28@gmail.com>
2016-02-08 18:36:18 -0200

    Add fleet and flannel services to coreos scope

--- scope/coreos/coreos.go
+++ scope/coreos/coreos.go
@@ -3,6 +3,8 @@ package coreos
 import (
 	"github.com/bernardolins/clustereasy/scope"
 	"github.com/bernardolins/clustereasy/service/etcd"
+	"github.com/bernardolins/clustereasy/service/flannel"
+	"github.com/bernardolins/clustereasy/service/fleet"
 	"github.com/bernardolins/clustereasy/setup/types"
 )
 
@@ -10,9 +12,17 @@ func CreateScope(node types.Node, cluster types.Cluster) *scope.Scope {
 	etcd2 := etcd2.New()
 	etcd2.Configure(node, cluster)
 
+	fleet := fleet.New()
+	fleet.Configure(node, cluster)
+
+	flannel := flannel.New()
+	flannel.Configure(node, cluster)
+
 	coreos := scope.New("coreos")
 
 	coreos.AddService(*etcd2)
+	coreos.AddService(*fleet)
+	coreos.AddService(*flannel)
 
 	return coreos
 }
