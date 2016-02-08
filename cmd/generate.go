9dff7dfe69d13749fbfc1a24081718d1c23d02fd
Bernardo Lins <bernardolins28@gmail.com>
2016-02-08 01:26:20 -0200

    Add input validation

--- cmd/generate.go
+++ cmd/generate.go
@@ -17,6 +17,7 @@ func init() {
 type GenerateCommand struct {
 	Command *cobra.Command
 	input   string
+	output  string
 	units   string
 }
 
@@ -27,15 +28,16 @@ func New() *GenerateCommand {
 		Use: "generate",
 		Run: func(cmd *cobra.Command, args []string) {
 			if err := generate.validateInput(); err != nil {
-				generate.run()
-			} else {
 				log.Fatalf("%v", err)
 				os.Exit(1)
+			} else {
+				generate.run()
 			}
 		},
 	}
 
 	generate.Command.Flags().StringVarP(&generate.input, "input", "i", "", "Path to input file")
+	generate.Command.Flags().StringVarP(&generate.output, "output", "o", "./", "Path to store output files")
 	generate.Command.Flags().StringVarP(&generate.units, "units", "u", "", "Path to units")
 
 	return generate
@@ -50,7 +52,7 @@ func (generate *GenerateCommand) run() {
 
 func (generate *GenerateCommand) validateInput() error {
 	if generate.input == "" {
-		return fmt.Errorf("Input file path missing!")
+		return fmt.Errorf("Input file path missing!\nTry something like 'cluectl generate -i /some/path/to/file.yml'")
 	}
 
 	return nil
