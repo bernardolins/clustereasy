b4758fddb956e7f843e6fc59a8aa1189b2ad5126
Bernardo Lins <bernardolins28@gmail.com>
2016-02-09 19:11:14 -0200

    Remove comments from service file

--- service/service.go
+++ service/service.go
@@ -4,31 +4,6 @@ import (
 	"github.com/bernardolins/clustereasy/setup/types"
 )
 
-//type Service struct {
-//	name   string
-//	fields []Field
-//}
-//
-//func NewService(name string) *Service {
-//	s := new(Service)
-//
-//	s.name = name
-//
-//	return s
-//}
-//
-//func (s *Service) AddField(field Field) {
-//	s.fields = append(s.fields, field)
-//}
-//
-//func (s *Service) GetName() string {
-//	return s.name
-//}
-//
-//func (s *Service) GetFields() []Field {
-//	return s.fields
-//}
-
 type Service interface {
 	GetName() string
 	GetParameters() map[string]string
