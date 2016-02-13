9eee877e17bb3e29ca2c1bf9a4639ef6b23af1a2
Bernardo Lins <bernardolins28@gmail.com>
2016-02-13 15:24:44 -0200

    create etcd2 service

--- cloudconfig/keys/coreos/etcd2/etcd2.go
+++ cloudconfig/keys/coreos/etcd2/etcd2.go
@@ -6,62 +6,82 @@ import (
 )
 
 type Etcd2 struct {
-	Name       string
-	Parameters map[string]string
+	name       string
+	parameters map[string]string
+	clientPort int
+	peerPort   int
+	useHttps   bool
 }
 
-func New() *Etcd2 {
+func Configure(node types.Node, cluster types.Cluster) Etcd2 {
 	etcd2 := new(Etcd2)
+	etcd2.name = "etcd2"
+	etcd2.parameters = make(map[string]string)
 
-	etcd2.Name = "etcd2"
-	etcd2.Parameters = make(map[string]string)
+	etcd2.clientPort = 2379
+	etcd2.peerPort = 2380
 
-	return etcd2
+	etcd2.configureMachine(node)
+	etcd2.configureCluster(cluster)
+
+	etcd2.useHttps = false
+
+	return *etcd2
 }
 
-func (etcd2 Etcd2) GetName() string {
-	return etcd2.Name
+func (etcd2 Etcd2) Name() string {
+	return etcd2.name
 }
 
-func (etcd2 Etcd2) GetParameters() map[string]string {
-	return etcd2.Parameters
+func (etcd2 Etcd2) Parameters() map[string]string {
+	return etcd2.parameters
 }
 
-func (etcd2 Etcd2) Configure(node types.Node, cluster types.Cluster) {
-	etcd2.configureClusterParameters(cluster)
-	etcd2.configureMachineParameters(node)
+// --------------------
+
+func (etcd2 Etcd2) configureCluster(cluster types.Cluster) {
+	etcd2.parameters["initial-cluster-token"] = cluster.GetToken()
+	etcd2.parameters["initial-cluster-state"] = cluster.GetState()
+
+	etcd2.setInitialClusterString(cluster)
 }
 
-func (etcd2 Etcd2) configureClusterParameters(cluster types.Cluster) {
-	etcd2.Parameters["initial-cluster-token"] = cluster.GetToken()
-	etcd2.Parameters["initial-cluster-state"] = cluster.GetState()
-
-	etcd2.Parameters["initial-cluster"] = setInitialClusterString(cluster)
+func (etcd2 Etcd2) configureMachine(node types.Node) {
+	etcd2.parameters["name"] = node.NodeName()
+	etcd2.parameters["initial-advertise-peer-urls"] = httpAddress(node.NodeIp(), etcd2.peerPort)
+	etcd2.parameters["listen-peer-urls"] = httpAddress(node.NodeIp(), etcd2.peerPort)
+	etcd2.parameters["listen-client-urls"] = httpAddress("0.0.0.0", etcd2.clientPort)
+	etcd2.parameters["advertise-client-urls"] = httpAddress(node.NodeIp(), etcd2.clientPort)
 }
 
-func setInitialClusterString(cluster types.Cluster) string {
+func (etcd2 Etcd2) setInitialClusterString(cluster types.Cluster) {
 	initialClusterString := ""
 
 	for _, node := range cluster.GetNodes() {
+		nodeString := ""
+
 		if initialClusterString != "" {
 			initialClusterString = initialClusterString + ","
 		}
 
-		nodeString := node.NodeName() + "=http://" + node.NodeIp() + ":2380"
+		if etcd2.useHttps {
+			nodeString = node.NodeName() + "=" + httpsAddress(node.NodeIp(), etcd2.peerPort)
+		} else {
+			nodeString = node.NodeName() + "=" + httpAddress(node.NodeIp(), etcd2.peerPort)
+		}
+
 		initialClusterString = initialClusterString + nodeString
 	}
 
-	return initialClusterString
+	etcd2.parameters["initial-cluster"] = initialClusterString
 }
 
-func (etcd2 Etcd2) configureMachineParameters(node types.Node) {
-	etcd2.Parameters["name"] = node.NodeName()
-	etcd2.Parameters["initial-advertise-peer-urls"] = httpAddress(node.NodeIp(), 2380)
-	etcd2.Parameters["listen-peer-urls"] = httpAddress(node.NodeIp(), 2380)
-	etcd2.Parameters["listen-client-urls"] = httpAddress(node.NodeIp(), 2379) + "," + httpAddress(node.NodeIp(), 2379)
-	etcd2.Parameters["advertise-client-urls"] = httpAddress(node.NodeIp(), 2379)
-}
+// -----------------------------
 
 func httpAddress(ip string, port int) string {
 	return "http://" + ip + ":" + strconv.Itoa(port)
+}
+
+func httpsAddress(ip string, port int) string {
+	return "https://" + ip + ":" + strconv.Itoa(port)
 }
